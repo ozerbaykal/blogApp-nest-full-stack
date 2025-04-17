c626ba861dd2390ee10923eb52ba80181e9f235f
ozerbaykal <baykalozer87@gmail.com>
2025-04-18 02:04:56 +0300

    add register login refresh and logput endpoint to authcontroller

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  register(@Body() createUserDto: any) {
+    return this.authService.register(createUserDto);
+  }
+
+  @Post('login')
+  login(@Body() loginDto: any) {
+    return this.authService.login(loginDto);
+  }
+
+  @Post('refresh')
+  refresh(@Body() refreshTokenDto: any) {
+    return this.authService.refresh(refreshDto);
+  }
+
+  @Post('logout')
+  logout(@Body() logoutDto: any) {
+    return this.authService.logout(logoutDto);
+  }
+}
