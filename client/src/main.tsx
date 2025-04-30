47b412781c51830e3699282a1e4ff516bdeb9c0c
ozerbaykal <baykalozer87@gmail.com>
2025-04-30 12:09:27 +0300

    add service utils and types

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import AuthProvider from "./providers/auth-provider.tsx";
+import { ToastContainer } from "react-toastify";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <AuthProvider>
     <App />
-  </StrictMode>,
-)
+    <ToastContainer />
+  </AuthProvider>
+);
---
+++ client/src/services/axios.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:3000",
+  withCredentials: true,
+  headers: {
+    "Content-Type ": "  application/json",
+  },
+});
+// eğer refresh tokenın süresi dolduysa yeni bir access token al
+// gelen cevaptaki hatayı kontrol edip refresh endpointine istek at
+// eğer refresh token'ında süresi dolduysa login sayfasına yönlendir
+
+export default api;
