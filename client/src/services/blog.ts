c92c2fbca7b953a37b176555519d9248f63b1e7f
ozerbaykal <baykalozer87@gmail.com>
2025-05-03 00:33:25 +0300

    complete blogService

--- client/src/services/blog.ts
+++ client/src/services/blog.ts
@@ -1,10 +1,26 @@
-import { GetAllParams, GetAllResponse } from "../types";
+import { Blog, CreateBlogValues, GetAllParams, GetAllResponse } from "../types";
 import api from "./axios";
 
 const blogService = {
   getAll: async (params: GetAllParams) => {
     const response = await api.get<GetAllResponse[]>("/posts", { params });
 
+    return response.data;
+  },
+  getById: async (id: string) => {
+    const response = await api.get<Blog>(`/posts/${id}`);
+
+    return response.data;
+  },
+  create: async (values: CreateBlogValues) => {
+    const response = await api.post<Blog>("/posts", values);
+  },
+  update: async (id: string, values: Partial<CreateBlogValues>) => {
+    const response = await api.patch<Blog>(`/posts/${id}`, values);
+    return response.data;
+  },
+  delete: async (id: string) => {
+    const response = await api.delete<Blog>(`/posts/${id}`);
     return response.data;
   },
 };
